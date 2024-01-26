e157710a51f9b743b522bc7c7b7dc19113497f2a
legiorex <batyushev@gmail.com>
2024-01-26 13:58:07 +0300

    create top banner

---
+++ src/widgets/TopBanner/Buttons/index.tsx
@@ -0,0 +1,24 @@
+import { FC, useState } from 'react'
+
+import Arrow from 'assets/arrow.svg?react'
+import Close from 'assets/close.svg?react'
+
+import MenuModal from 'components/MenuModal'
+
+import s from './s.module.scss'
+
+const Buttons: FC = () => {
+  const [isOpen, setOpen] = useState(false)
+
+  return (
+    <>
+      <div className={s.buttons}>
+        <button className={s.buttonShop}>Shop now</button>
+        <Close />
+      </div>
+      <Arrow className={s.menu} onClick={() => setOpen(true)} />
+      <MenuModal isOpen={isOpen} onClose={() => setOpen(false)} />
+    </>
+  )
+}
+export default Buttons
